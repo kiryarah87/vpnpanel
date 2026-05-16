347bf3e79c4690560ebc29fcb1296891b5d95fd1
Кирилл Рахно <krakhno@aflt-systems.ru>
2026-05-16 18:28:32 +0300

    Fix deploy on vps

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def create_app() -> FastAPI:
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost"],
+        allow_origins=["http://localhost", "http://localhost:8080"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
